353a48499a552840912501c8892a8d681886f615
Hyu <liliumhyu@gmail.com>
2017-10-16 00:07:20 +0300

    Add project files.

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,12 @@
+//this program outputs the message "Hello, World!" to the monitor
+
+#include <iostream>
+using namespace std;
+
+int main() //c++ programs start by executing the function main
+{
+	cout << "Hello, World!\n"; // output "Hello, World!"
+	system("pause");
+	return 0;
+}
+
